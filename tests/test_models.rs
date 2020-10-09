d2dfaf70d63bc4bddc48ed4b9e0a70ab25eff370
Yabir Garcia <yabirg@protonmail.com>
2020-10-09 11:17:25 +0200

    añade más tests de los metodos

--- tests/test_models.rs
+++ tests/test_models.rs
@@ -29,3 +29,99 @@ fn test_modifiers_debtor(){
 
     assert_eq!(new_debtor.paid, false, "Failed to toggle paid true to false");
 }
+
+#[test]
+fn test_account_add_debtor(){
+    let mut acc = models::Account{
+        items: Vec::new(),
+        debtors: Vec::new(),
+    };
+
+    let debtor = models::Debtor{
+        name: "Debtor 1".to_string(),
+        id: "21323".to_string(),
+        paid_amount: 0.0,
+        fraction: 0.5,
+        paid: false
+    };
+
+    acc.add_debtor(debtor);
+
+    assert_eq!(acc.debtors.len(), 1);
+}
+
+#[test]
+fn test_account_add_item(){
+    let mut acc = models::Account{
+        items: Vec::new(),
+        debtors: Vec::new(),
+    };
+
+    let item = models::Item{
+        name: "Item 1".to_string(),
+        date: 12312321,
+        price: 23.0
+    };
+
+    acc.add_item(item.clone());
+    acc.add_item(item.clone());
+
+    assert_eq!(acc.items.len(), 2);
+}
+
+#[test]
+fn test_total_debt_no_debtors(){
+    let mut acc = models::Account{
+        items: Vec::new(),
+        debtors: Vec::new(),
+    };
+
+    let item = models::Item{
+        name: "Item 1".to_string(),
+        date: 12312321,
+        price: 23.0
+    };
+
+    acc.add_item(item.clone());
+    acc.add_item(item.clone());
+
+    assert_eq!(acc.total_debt(), 46.0);
+}
+
+#[test]
+fn test_total_price_debtor(){
+    let mut acc = models::Account{
+        items: Vec::new(),
+        debtors: Vec::new(),
+    };
+
+    let item = models::Item{
+        name: "Item 1".to_string(),
+        date: 12312321,
+        price: 23.0
+    };
+
+    let debtor = models::Debtor{
+        name: "Debtor 1".to_string(),
+        id: "21323".to_string(),
+        paid_amount: 22.0,
+        fraction: 0.5,
+        paid: false
+    };
+
+    acc.add_item(item.clone());
+    acc.add_item(item.clone());
+    acc.add_debtor(debtor.clone());
+
+    assert_eq!(acc.total_debt(), 24.0);
+}
+
+#[test]
+fn test_total_price_empty(){
+    let acc = models::Account{
+        items: Vec::new(),
+        debtors: Vec::new(),
+    };
+
+    assert_eq!(acc.total_debt(), 0.0);
+}
