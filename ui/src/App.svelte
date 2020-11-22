80413ae3df52ab9cba12038e41319ef0d93e71db
Yabir Garcia <yabirg@protonmail.com>
2020-11-22 14:05:31 +0100

    Añade landing y colaboradores #61

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -7,33 +7,71 @@
 <Tailwindcss />
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<Collaborators/>
+	<nav id="header" class="fixed w-full z-30 top-0 text-white bg-white">
+      <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+        <div class="pl-4 flex items-center">
+          <a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
+            Bukhgalter
+          </a>
+        </div>
+        <div class="block lg:hidden pr-4">
+          <button id="nav-toggle" class="flex items-center p-1 text-orange-800 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+            <svg class="fill-current h-6 w-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20" id="nav-content">
+          <ul class="list-reset lg:flex justify-end flex-1 items-center">
+            <li class="mr-3">
+              <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="https://github.com/yabirgb/bukhgalter">Github</a>
+            </li>
+            <li class="mr-3">
+              <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="https://twitter.com/yabirgb">Twitter</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <hr class="border-b border-gray-100 opacity-25 my-0 py-0" />
+    </nav>
+    <!--Hero-->
+    <div class="pt-24">
+      <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+        <!--Left Col-->
+        <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
+          <h1 class="my-4 text-5xl font-bold leading-tight">
+            Nunca fue tan fácil hacer planes juntos
+          </h1>
+          <p class="leading-normal text-2xl mb-8">
+            Que repartir gastos entre amigos no sea un impedimento
+          </p>
+          <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+            Empieza ahora
+          </button>
+        </div>
+        <!--Right Col-->
+        <div class="w-full md:w-3/5 py-6 text-center">
+          <img class="w-full md:w-4/5 z-50" src="/images/shared.jpg" />
+        </div>
+      </div>
+    </div>
+	<section class="bg-white border-b py-8">
+      <div class="container mx-auto flex flex-wrap pt-4 pb-12">
+        <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+          Elige ayudar
+        </h1>
+		<p class="leading-normal text-2xl mb-8 w-full text-center">Al hacer cuentas con tus amig@s también puedes elegir ayudar a uno de los proyectos que colaboran con nosotros</p>
+        <div class="w-full mb-4">
+          <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+        </div>
+		<Collaborators></Collaborators>
+      </div>
+    </section>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-
-	h1 {
-  	  @apply bg-black text-white;
+	.gradient {
+	background: linear-gradient(90deg, #d53369 0%, #daae51 100%);
 	}
 </style>
--- ui/src/views/Collaborators.svelte
+++ ui/src/views/Collaborators.svelte
@@ -6,7 +6,7 @@
   let collaborators = [];
 
 	onMount(() => {
-    var urly = 'http://localhost:3000/api/collaborators'      
+    var urly = 'https://bukhgalter-collaborators.vercel.app/api/collaborators'      
     fetch(urly)
       .then(res => res.json())
       .then(data => {
@@ -17,11 +17,17 @@
 
 </script>
 
-<div class="container-home">
-  
-
-  {#each collaborators as collaborator}
-     {collaborator}
-  {/each}
-
-</div>
+{#each collaborators as collaborator}
+      <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+        <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+          <a href="#" class="flex flex-wrap no-underline hover:no-underline">
+            <div class="w-full font-bold text-xl text-gray-800 px-6">
+              {collaborator.name}
+            </div>
+            <p class="text-gray-800 text-base px-6 mb-5">
+              {collaborator.description}
+            </p>
+          </a>
+        </div>
+      </div>
+{/each}
