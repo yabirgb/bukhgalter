db38ff123e225ea0eb3b4c268fbee8dd0e3491fe
Yabir Garcia <yabirg@protonmail.com>
2020-11-27 20:18:32 +0100

    Add warp as dependenci

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 //use mongodb::{Client, options::ClientOptions};
 use std::env;
 use std::error::Error;
+use warp::http::StatusCode;
+use warp::Filter;
+use serde::Serialize;
 //use tokio;
 
 mod models;
 
-fn main(){
-    println!("hello from bukhgalter");
+#[derive(Debug, Serialize)]
+struct Health{
+    status: String
+}
+
+#[tokio::main]
+async fn main(){
+
+    let health_route = warp::path("status")
+        .map(|| warp::reply::json(&Health{status:"OK".to_string()}));
+
+    let routes = health_route
+        .with(warp::cors().allow_any_origin());
+
+    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
 }
